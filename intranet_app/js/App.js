966446fa2a4dd74008db25eaddd4b96cc0bc9a4d
Mike Gallagher <mgallagher@cboe.com>
2018-02-22 12:15:14 -0500

    Some quick cleanup

--- intranet_app/js/App.js
+++ intranet_app/js/App.js
@@ -4,16 +4,34 @@ import ReactDOM from 'react-dom';
 import { Button } from 'Components/Button';
 import { Divider } from 'Components/Divider';
 
-const App = ({ title }) => (
-  <div>
-    <h2>{title}</h2>
-    <Divider width={3} color="lightgrey" />
-    <Divider width={3} color="lightblue" />
-    <Button title="Do Nothing" />
-  </div>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        };
+    }
 
-ReactDOM.render(
-  <App title="Wow React" />,
-  document.getElementById('root')
-);
+    incrementCounter = () => {
+        this.setState({ counter: this.state.counter + 1 });
+    };
+
+    resetCounter = () => {
+        this.setState({ counter: 0 });
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>{this.props.title}</h2>
+                <Button title="Increment" onClickHandler={this.incrementCounter} />
+                <Button title="Reset" onClickHandler={this.resetCounter} />
+                <Divider width={3} color="lightgrey" />
+                <h1>{this.state.counter}</h1>
+                <Divider width={3} color="lightblue" />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App title="Wow React" />, document.getElementById('root'));
