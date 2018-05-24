eb2113acf0c74a24b0e9f1f421d8a859fa96cbc1
Mike Gallagher <mgallagher@cboe.com>
2018-05-24 12:47:14 -0500

    Removing hard-coded hostname from the HTML template

--- intranet_app/views.py
+++ intranet_app/views.py
@@ -1,9 +1,16 @@
+from __future__ import print_function
+import os
+
 from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def index(request):
-	return render(request, 'index.html')
+    hostname = os.environ['HOST']
+    webpack_dev_server_url = 'http://{host}:{port}/intranet_app/js/bundle.js'.format(host=hostname, port=9000)
+    print('*** HOSTNAME: ' + hostname + ' ***')
+    print('*** WEBPACK DEV SERVER URL: ' + webpack_dev_server_url + ' ***')
+    return render(request, 'index.html', context={'webpack_dev_server_url': webpack_dev_server_url})
 
 
 def get_data(request):
